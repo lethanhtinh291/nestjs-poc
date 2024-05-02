3aa4ddc4c4f4b9bfabb29c13d93af0c47047cb45
vophitruonganh <truonganh.vo@popsww.com>
2024-05-02 08:15:25 +0700

    Update build spec

--- start_server.sh
+++ start_server.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 PROFILE=@PROFILE@
 PROJ_NAME=@PROJ_NAME@
-COMPANY_NAME=@COMPANY_NAME@
 TAG_VERSION=@TAG_VERSION@
 AWS_ACCOUNT_ID=@AWS_ACCOUNT_ID@
 AWS_DEFAULT_REGION=@AWS_DEFAULT_REGION@
 
-REPOSITORY_URL_TAG=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/private-ecr-${COMPANY_NAME}-${PROJ_NAME}:${PROFILE}_${TAG_VERSION}
+REPOSITORY_URL_TAG=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/poc-miraen/api-poc-miraen:${PROFILE}_${TAG_VERSION}
 
 if [ "$PROFILE" = "dev" ]; then
     I_PORT=3000
@@ -17,6 +16,5 @@ fi
 sudo docker run \
 -d \
 -p ${I_PORT}:3000 \
---env-file /home/ec2-user/.env \
 --name ${PROJ_NAME} \
 ${REPOSITORY_URL_TAG}
