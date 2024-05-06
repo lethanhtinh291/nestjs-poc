fae32127a981db8f657d54c5614282a87d4c6dbe
vophitruonganh <truonganh.vo@popsww.com>
2024-05-06 14:14:54 +0700

    Set image version after build image

--- start_server.sh
+++ start_server.sh
@@ -2,7 +2,6 @@
 PROFILE=@PROFILE@
 IMAGE_VERSION=@IMAGE_VERSION@
 IMAGE_REPO_URL="809695185788.dkr.ecr.ap-southeast-2.amazonaws.com/"
-IMAGE_VERSION="latest"
 IMAGE_APP_NAME="poc-miraen"
 AWS_DEFAULT_REGION="ap-southeast-2"
 I_PORT=3000
